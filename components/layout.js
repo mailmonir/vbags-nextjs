9d76274cb66269231a5ddbe0212d24ce01fa464c
Monirul Islam <mailmonir@gmail.com>
2023-01-28 11:42:31 +0600

    loading component added

--- components/layout.js
+++ components/layout.js
@@ -1,11 +1,20 @@
+import { useState, useEffect } from "react";
 import Head from "next/head";
 import Navbar from "./navbar";
 import Footer from "./footer";
 import ScrollTop from "./ScrollTop";
 import WhatsApp from "./WhatsApp";
+import LoadingSpinner from "./LoadingSpinner"
 
 const Layout = ({ children, header, footer, info }) => {
+  const [loading, setLoading] = useState(true)
+  useEffect(() => {
+    setLoading(false)
+  }, [])
   return (
+    loading ? (
+      <LoadingSpinner />
+    ) : (
     <div>
       <Head>
         <link rel="icon" href="/favicon/favicon.ico" />
@@ -20,7 +29,7 @@ const Layout = ({ children, header, footer, info }) => {
       <ScrollTop />
       <WhatsApp info={info} />
     </div>
-  );
+  ));
 };
 
 export default Layout;
---
+++ components/loadingSpinner.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { CgSpinnerTwo } from "react-icons/cg"
+
+const LoadingSpinner = () => {
+  return (
+    <div className="lspinner">
+      <span>
+        <CgSpinnerTwo />
+      </span>
+    </div>
+  )
+}
+
+export default LoadingSpinner
+              
